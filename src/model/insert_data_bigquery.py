e86eae0e29a80ee4cec56ef3b1ac8908d1aedcae
Jeferson Andrade <jandrademelo@yahoo.com.br>
2025-08-26 16:57:06 -0300

    Atualização de código para evitar Warnings
    Ao executar o script, surgia o warning informando sobre a descontinuação do pandas.to_gbq. Foi feita a alteração para o novo formato, sendo apenas o to_gbq().

--- src/model/insert_data_bigquery.py
+++ src/model/insert_data_bigquery.py
@@ -2,7 +2,7 @@ import pandas as pd
 from google.cloud import bigquery
 from google.oauth2 import service_account
 from pandas_gbq import to_gbq
-
+import os
 
 
 def insert_data(dim_funcionarios, dim_escritorios, dim_cidades, dim_cargos, dim_area, dim_nivel, dim_desempenho, dim_remuneracao, dim_emprego):
@@ -17,30 +17,30 @@ def insert_data(dim_funcionarios, dim_escritorios, dim_cidades, dim_cargos, dim_
     dataset = "INSIRA O NOME DO DATASET NO GOOGLE BIGQUERY AQUI"
     
     # Inserindo dados em cada tabela do BigQuery
-    # dim_funcionarios.to_gbq(f'{dataset}.dim_funcionarios', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_funcionarios.to_gbq(f'{dataset}.dim_funcionarios', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_funcionarios, f'{dataset}.dim_funcionarios', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_funcionarios, f'{dataset}.dim_funcionarios', project_id=projeto, if_exists='replace', credentials=credentials)
     
-    # dim_escritorios.to_gbq(f'{dataset}.dim_escritorios', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_escritorios.to_gbq(f'{dataset}.dim_escritorios', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_escritorios, f'{dataset}.dim_escritorios', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_escritorios, f'{dataset}.dim_escritorios', project_id=projeto, if_exists='replace', credentials=credentials)
     
-    # dim_cidades.to_gbq(f'{dataset}.dim_cidades', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_cidades.to_gbq(f'{dataset}.dim_cidades', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_cidades, f'{dataset}.dim_cidades', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_cidades, f'{dataset}.dim_cidades', project_id=projeto, if_exists='replace', credentials=credentials)
     
-    # dim_cargos.to_gbq(f'{dataset}.dim_cargos', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_cargos.to_gbq(f'{dataset}.dim_cargos', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_cargos, f'{dataset}.dim_cargos', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_cargos, f'{dataset}.dim_cargos', project_id=projeto, if_exists='replace', credentials=credentials)
     
-    # dim_area.to_gbq(f'{dataset}.dim_area', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_area.to_gbq(f'{dataset}.dim_area', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_area, f'{dataset}.dim_area', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_area, f'{dataset}.dim_area', project_id=projeto, if_exists='replace', credentials=credentials)
     
-    # dim_nivel.to_gbq(f'{dataset}.dim_nivel', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_nivel.to_gbq(f'{dataset}.dim_nivel', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_nivel, f'{dataset}.dim_nivel', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_nivel, f'{dataset}.dim_nivel', project_id=projeto, if_exists='replace', credentials=credentials)
     
-    # dim_desempenho.to_gbq(f'{dataset}.dim_desempenho', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_desempenho.to_gbq(f'{dataset}.dim_desempenho', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_desempenho, f'{dataset}.dim_desempenho', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_desempenho, f'{dataset}.dim_desempenho', project_id=projeto, if_exists='replace', credentials=credentials)
     
-    # dim_remuneracao.to_gbq(f'{dataset}.dim_remuneracao', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_remuneracao.to_gbq(f'{dataset}.dim_remuneracao', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_remuneracao, f'{dataset}.dim_remuneracao', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_remuneracao, f'{dataset}.dim_remuneracao', project_id=projeto, if_exists='replace', credentials=credentials)
     
-    # dim_emprego.to_gbq(f'{dataset}.dim_emprego', project_id=projeto, if_exists='append', credentials=credentials)
-    dim_emprego.to_gbq(f'{dataset}.dim_emprego', project_id=projeto, if_exists='replace', credentials=credentials)
+    # to_gbq(dim_emprego, f'{dataset}.dim_emprego', project_id=projeto, if_exists='append', credentials=credentials)
+    to_gbq(dim_emprego, f'{dataset}.dim_emprego', project_id=projeto, if_exists='replace', credentials=credentials)
 
